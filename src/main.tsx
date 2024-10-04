d2bd03cd72464361f5caedf72489192d31315d85
Giovanni Ferrara <gianmarco.ferrara@gmail.com>
2024-10-04 21:45:34 +0200

    no strict

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,11 @@
 // src/main.jsx
-import React from "react";
 import ReactDOM from "react-dom/client";
 import AppProviders from "./app-providers";
 import App from "./app/app";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <AppProviders>
-      <App />
-    </AppProviders>
-  </React.StrictMode>
+  <AppProviders>
+    <App />
+  </AppProviders>
 );
